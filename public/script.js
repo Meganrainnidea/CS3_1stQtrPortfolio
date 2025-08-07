edbab1d3956ea2f9825928127531e997024232e4
Meganrainnidea <b2029mrfnidea@pshs.edu.ph>
2025-08-07 10:36:46 +0800

    1st non graded exercise

---
+++ public/script.js
@@ -0,0 +1,9 @@
+let age= 14;
+let maxAge= 18;
+let numPerDay= 6;
+totalRequired = (numPerDay * 365) * (maxAge - age) 
+
+document.write("You will need" totalRequired " to last you until the ripe old age of" age);
+
+    
+
