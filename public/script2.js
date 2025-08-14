d61ca4c1dca26dab95bca38fbc0bcb25fe16f07a
Meganrainnidea <b2029mrfnidea@pshs.edu.ph>
2025-08-14 10:08:47 +0800

    Commit ver3

---
+++ public/script2.js
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        </head>
+        <body>
+            <!--comment line-->
+            <h1> Feed me My Snacks</h1>
+            <label> Enter your age
+                <input id="age"/>
+            
+            </label> <br/>
+            <label> Enter your Max age
+                <input id="age"/>
+                </label><br/>
+            <label>
+                Enter number of Snacks
+                <input id="numPerDay"/>
+                </label><br/>
+                <p> Result :
+                    <span id="resultHere"></span>
+                    </p>
+                    <button>Accept input </button>
+                    <script src= "script2.js"></script>
+
+                </p>
+            </label>
+        </body>
+    </head>
+</html>
